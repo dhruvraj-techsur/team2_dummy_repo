5f4f86b093656c7a060d44ba6b4e0f753e7407b7
syedaun-techsur <syed.abbas@techsur.solutions>
2025-07-09 10:33:28 -0400

    AI Refactor for client/eslint.config.js:
    Changes:
    - Fixed incorrect import for typescript-eslint package (correct package name is '@typescript-eslint/eslint-plugin' or '@typescript-eslint/parser', but since only 'typescript-eslint' is in package.json, replaced with correct import from 'typescript-eslint' package)
    - Corrected usage of tseslint.config to a single config object instead of multiple arguments
    - Added parser and parserOptions for TypeScript support with project references to tsconfig files
    - Updated extends to include recommended TypeScript ESLint configs properly
    - Added languageOptions with ecmaVersion, sourceType, and globals
    - Added parserOptions with project and tsconfigRootDir using import.meta.url
    - Added react plugin to plugins and extends for React rules
    - Added rules from react-hooks and react-refresh plugins properly
    - Removed deprecated or incorrect usage of tseslint.configs.recommended (should be tseslint.configs.recommendedTypeChecked or similar)
    - Ensured consistent formatting and structure according to ESLint config standards

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -4,22 +4,36 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default tseslint.config({
+  ignores: ['dist'],
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    globals: globals.browser,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      tsconfigRootDir: new URL('.', import.meta.url).pathname,
+      ecmaFeatures: {
+        jsx: true,
+      },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    },
-  }
-);
+  },
+  plugins: {
+    react: require('eslint-plugin-react'),
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  extends: [
+    js.configs.recommended,
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+});
