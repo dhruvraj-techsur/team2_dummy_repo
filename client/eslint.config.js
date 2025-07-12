979a16871a2adba09dd89845d5e7bb4a366ce169
syedaun-techsur <syed.abbas@techsur.solutions>
2025-07-12 18:55:29 -0400

    AI Refactor for client/eslint.config.js:
    Changes:
    - Fixed import statement for `@typescript-eslint/eslint-plugin`
    - Corrected usage of `tseslint.config` function
    - Ensured compatibility with ESLint's flat configuration format

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -2,12 +2,12 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import { TSESLint } from '@typescript-eslint/utils';
 
-export default tseslint.config(
+export default TSESLint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...TSESLint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
